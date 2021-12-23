342e12c7d4cf7649351affb83824c1d78bdf97c6
Brian <bwilliams7622@gmail.com>
2021-12-22 22:02:40 -0600

    server now runs on port 3000; routes module created and exported

---
+++ assets/app/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const myRoutes = require('./routes')
+const app = express()
+
+app.use('/' , myRoutes)
+
+app.listen(3000 , error => {
+    console.log('app currently running @ http://localhos:3000; press ctrl+c to cancel.')
+})
